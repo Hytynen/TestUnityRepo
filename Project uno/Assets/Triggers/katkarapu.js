45c6cae6b43ba00b5882779a76dc4ee717c1df61
Cuuleri <jimi_kamu@hotmail.com>
2013-04-16 11:22:39 +0300

    V 0.02

--- Project uno/Assets/Triggers/katkarapu.js
+++ Project uno/Assets/Triggers/katkarapu.js
@@ -14,9 +14,11 @@ function OnTriggerEnter(collision : Collider) {
 		return;
 	} else if(Triggered==false){
 	yourVar.range=0;
+	transform.Rotate(0, 180, 0, Space.Self);
 	Triggered=true;
 	}
 	else if(Triggered==true){
 	yourVar.range=50;
+	transform.Rotate(0, 180, 0, Space.Self);
 	Triggered=false;}
 }
